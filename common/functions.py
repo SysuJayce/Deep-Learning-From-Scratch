b68933232c0e5754f08440a1c879a9aa99be0f23
Jayce Wong <sysusshyj@gmail.com>
2018-08-04 21:16:10 +0800

    增加均方误差和交叉熵误差损失函数

--- common/functions.py
+++ common/functions.py
@@ -34,3 +34,28 @@ def softmax(x):
 
     x = x - np.max(x)  # 为防止指数计算时溢出，减去最大值
     return np.exp(x) / np.sum(np.exp(x))
+
+
+def mean_squared_error(true_label, prediction):
+    return 0.5 * np.sum((true_label-prediction)**2)
+
+
+def cross_entropy_error(true_label, prediction):
+    delta = 1e-7  # 设置一个极小值来防止计算ln(0)时溢出下界
+
+    # 维度为1代表向量(array数组)，维度为2代表矩阵
+    # 这里将向量转换成矩阵形式
+    if prediction.ndim == 1:
+        true_label = true_label.reshape(1, true_label.size)
+        prediction = prediction.reshape(1, prediction.size)
+
+    # 如果预测值和真实值的大小一样，说明真实值是以one-hot编码的
+    # 因此将真实值转换成one-hot编码的索引
+    # 这时true_label表示预测值array中用于计算熵大小的下标
+    # 即 -sum(ln(pred[true_label]))
+    if true_label.size == prediction.size:
+        true_label = true_label.argmax(axis=1)
+
+    batch_size = prediction.shape[0]  # 如果是以batch进行训练就求平均误差
+    return -np.sum(np.log(prediction[np.arange(batch_size), true_label] +
+                          delta)) / batch_size
