6a3ea6016bf058340606a56727d46c881a514bc2
Jayce Wong <sysusshyj@gmail.com>
2018-08-11 00:12:01 +0800

    增加一个用于打乱数据集的函数

--- common/util.py
+++ common/util.py
@@ -20,3 +20,18 @@ def smooth_curve(x):
     w = np.kaiser(window_len, 2)
     y = np.convolve(w/w.sum(), s, mode='valid')
     return y[5:len(y)-5]
+
+
+def shuffle_dataset(x, t):
+    """
+    使用numpy.random中的permutation函数来实现数据集的打乱
+    效果和我之前自己实现的get_data是一样的，不过这个函数是直接打乱数据集，而我是
+    不打乱数据集，而是打乱下标。感觉我的做法效率应该更高，不过实现会比较麻烦
+    :param x: 特征
+    :param t: 标签
+    :return:
+    """
+    permutation = np.random.permutation(x.shape[0])  # 打乱下标
+    # 按打乱后的下标提取数据集，然后返回打乱后的新的数据集
+    x = x[permutation, :] if x.ndim == 2 else x[permutation, :, :, :]
+    t = t[permutation]
